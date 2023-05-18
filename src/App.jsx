a694cf746337e908b6785e0b4e0b5f90f9b75d32
Eadon Auston <97257951+EadonAuston@users.noreply.github.com>
2023-05-17 18:09:11 -0700

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,52 @@
+import { useState } from "react";
 import "./App.css";
 import { GameBoard } from "./Components/GameBoard";
 import { ScoreBoard } from "./Components/ScoreBoard";
 import "./Components/styles/final-score.css";
+import { FinalScore } from "./Components/FinalScore";
 
 function App() {
+  const [correctCount, setCorrectCount] = useState(0);
+  const [incorrectCount, setIncorrectCount] = useState(0);
+  const [answersLeft, setAnswersLeft] = useState([
+    "trout",
+    "salmon",
+    "shark",
+    "tuna",
+  ]);
+  const [page, setPage] = useState(1);
+  const totalCount = 4;
+  const getRidOfCorrectOption = (value) => {
+    const updatedAnswersLeft = answersLeft.filter((answer) => answer !== value);
+    setAnswersLeft(updatedAnswersLeft);
+  };
+
+  const goToResultsScreen = () => {
+    setPage(2);
+  };
+
   return (
     <div className="App">
       <header>
-        <ScoreBoard />
-        <GameBoard />
+        {page === 1 ? (
+          <>
+            <ScoreBoard
+              answersLeft={answersLeft}
+              correctCount={correctCount}
+              incorrectCount={incorrectCount}
+            />
+            <GameBoard
+              getRidOfCorrectOption={getRidOfCorrectOption}
+              setCorrectCount={setCorrectCount}
+              correctCount={correctCount}
+              setIncorrectCount={setIncorrectCount}
+              incorrectCount={incorrectCount}
+              goToResultsScreen={goToResultsScreen}
+            />
+          </>
+        ) : (
+          <FinalScore correctCount={correctCount} totalCount={totalCount} />
+        )}
       </header>
     </div>
   );
--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -1,5 +1,7 @@
 import "./styles/game-board.css";
 import { Images } from "../assets/images";
+import { useState } from "react";
+import { FinalScore } from "./FinalScore";
 
 const initialFishes = [
   {
@@ -20,17 +22,56 @@ const initialFishes = [
   },
 ];
 
-export const GameBoard = () => {
-  const nextFishToName = initialFishes[0];
+export const GameBoard = ({
+  getRidOfCorrectOption,
+  setCorrectCount,
+  correctCount,
+  setIncorrectCount,
+  incorrectCount,
+  goToResultsScreen,
+}) => {
+  const [fishToNameIndex, setFishToNameIndex] = useState(0);
+  const [guessInput, setGuessInput] = useState("");
+  const fishToName = initialFishes[fishToNameIndex];
+
+  const increaseFishIndex = () => {
+    setFishToNameIndex(fishToNameIndex + 1);
+  };
+
+  const makeAGuess = ({ target: { value } }) => {
+    setGuessInput(value);
+  };
+
+  const validateAnswer = () => {
+    fishToName.name === guessInput
+      ? setCorrectCount(correctCount + 1)
+      : setIncorrectCount(incorrectCount + 1);
+  };
 
   return (
     <div id="game-board">
       <div id="fish-container">
-        <img src={nextFishToName.url} alt={nextFishToName.name} />
+        <img src={fishToName.url} alt={fishToName.name} />
       </div>
-      <form id="fish-guess-form" onSubmit={(e) => {}}>
+      <form
+        id="fish-guess-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          validateAnswer();
+          setGuessInput("");
+          if (fishToNameIndex === 3) {
+            goToResultsScreen();
+          }
+          increaseFishIndex();
+          getRidOfCorrectOption(fishToName.name);
+        }}>
         <label htmlFor="fish-guess">What kind of fish is this?</label>
-        <input type="text" name="fish-guess" />
+        <input
+          type="text"
+          name="fish-guess"
+          onChange={makeAGuess}
+          value={guessInput}
+        />
         <input type="submit" />
       </form>
     </div>
